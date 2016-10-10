5d8ad6f495b51fab002b44b2f92d4f60bda09323
Manoj Chalode <chalodem@gmail.com>
2016-10-10 22:42:39 +0530

    added addition program

---
+++ addition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+
+	printf("Please enter first number \n");
+	scanf("%d", &a);
+
+	printf("Please enter second number \n");
+	scanf("%d", &b);
+
+	printf("The sum is - %d\t", (a+b));
+
+	return 0;
+}
